b3acf07de67763e9912d6e668e4666c5e3f0db6c
tocvic <vickytochi6882@gmail.com>
2023-07-10 01:16:44 +0100

    debugging

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-"""Type Checking"""
+""" A type-annotation of sum_list."""
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
-    """Return sum of list of floats
-
-    Args:
-        input_list (float): list of floats
-
-    Returns:
-        float: sum of list of floats
-    """
-    return sum(input_list)
+def sum_list(input_list: List[float]) -> float:
+    """ Return sum of list."""
+    return float(sum(input_list))
