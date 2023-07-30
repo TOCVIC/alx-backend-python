194fe591307e8a4fbe3946c14d7b3e1b818e97ef
tocvic <vickytochi6882@gmail.com>
2023-07-30 15:37:58 +0100

    debugging

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''Task 2's module.
-'''
-
-
-def floor(n: float) -> int:
-    """Returns the floor of the float"""
-    return int(n)
--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''Task 3's module.
-'''
-
-
-def to_str(n: float) -> str:
-    """Returns the string representation of the float"""
-    return str(n)
